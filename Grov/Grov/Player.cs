dac323dcf859cd64536dbb65f3ad708e69ddb796
Jake Zaia <jlz2369@g.rit.edu>
2019-02-18 20:12:26 -0500

    Add basic framework to Player class

--- Grov/Grov/Player.cs
+++ Grov/Grov/Player.cs
@@ -10,12 +10,66 @@ using Microsoft.Xna.Framework.Input;
 /*
  * Authors
  * Jack Hoffman
+ * Jake Zaia
  */
  
 namespace Grov
 {
     class Player: Creature
     {
+        // ************* Fields ************* //
+        private int maxMP;
+        private int currMP;
+        //private Weapon weapon;
+        private int keys;
+        private int bombs;
+
+
+        // ************* Properties ************* //
+
+        public int CurrMP
+        {
+            get => currMP;
+            set
+            {
+                if (value >= 0)
+                {
+                    currMP = value;
+                }
+                else
+                {
+                    currMP = 0;
+                }
+            }
+        }
+
+        public int MaxMP { get => maxMP; set => maxMP = value; }
+        public int Keys { get => keys; set => keys = value; }
+        public int Bombs { get => bombs; set => bombs = value; }
+
+
+        // ************* Constructor ************* //
+
+        public Player(int maxMP)
+        {
+            this.maxMP = maxMP;
+            this.currMP = maxMP;
+            keys = 0;
+            bombs = 0;
+        }
+
+
+        // ************* Methods ************* //
+
+        private void HandleInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
