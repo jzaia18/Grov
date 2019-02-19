2e0e1ef900f357228a0e2e48f045461685f55aa6
Jake Zaia <jlz2369@g.rit.edu>
2019-02-18 20:19:42 -0500

    Add basic framework to Enemy class

---
+++ Grov/Grov/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Authors: Jake Zaia
+
+namespace Grov
+{
+    enum EnemyType
+    {
+
+    }
+
+    class Enemy : Creature
+    {
+        // ************* Fields ************* //
+
+        private EnemyType enemyType;
+
+
+        // ************* Constructor ************* //
+
+        public Enemy()
+        {
+
+        }
+
+
+        // ************* Constructor ************* //
+
+        public void Update(Entity target)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Attack(Entity target)
+        {
+
+        }
+    }
+}
