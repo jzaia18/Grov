5b8e833c36391c72e0428f7dadd21a7b3d031b0f
Jake Zaia <jlz2369@g.rit.edu>
2019-02-21 21:11:03 -0500

    Begin work on Room class

---
+++ Grov/Grov/Room.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+// Authors: Jake Zaia
+
+namespace Grov
+{
+    enum RoomType
+    {
+        Normal,
+        Boss,
+        Shop,
+        treasure,
+        Secret
+    }
+
+    class Room
+    {
+        // ************* Fields ************* //
+
+        private Entrance top, bottom, left, right;
+        private RoomType type;
+        private bool isCleared;
+        private Tile[][] tiles;
+
+
+        // ************* Properties ************* //
+
+        public Entrance Top { get => top; set => top = value; }
+        public Entrance Bottom { get => bottom; set => bottom = value; }
+        public Entrance Left { get => left; set => left = value; }
+        public Entrance Right { get => right; set => right = value; }
+        public RoomType Type { get => type; set => type = value; }
+        public bool IsCleared { get => isCleared; set => isCleared = value; }
+
+        public Tile this[int x, int y]
+        {
+            get => tiles[x][y];
+            set => tiles[x][y] = value;
+        }
+
+
+        // ************* Constructor ************* //
+
+        public Room(RoomType type)
+        {
+            isCleared = false;
+            tiles = new Tile[32][];
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                tiles[i] = new Tile[18];
+            }
+            this.type = type;
+
+            ReadFromFile("test");
+        }
+
+
+        // ************* Methods ************* //
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        private void ReadFromFile(string filename)
+        {
+
+        }
+    }
+}
